136b59a644b0b5806994a273fd084e6c75d787f4
UnluckyNinja <UnluckyNinja@users.noreply.github.com>
2020-01-10 20:14:28 +0800

    add buefy

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import Buefy from 'buefy'
+
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
 
